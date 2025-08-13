31c41df91c7bb1d7958f9e4a89849ecbc4e5177a
SonaniAkshit <sonaniakshit684@gmail.com>
2025-08-13 08:23:49 +0530

    Remove commented line from sequences SQL script
    Deleted a commented line that was not needed in the 02.Sequences.sql file to clean up the script.

--- Movie Ticket Booking/Table Releted Objects/02.Sequences.sql
+++ Movie Ticket Booking/Table Releted Objects/02.Sequences.sql
@@ -7,6 +7,5 @@ create sequence seq_tickets start with 1 increment by 1;
 create sequence seq_payments start with 1 increment by 1;
 create sequence seq_admins start with 1 increment by 1;
 
--- select all created sequences in our schema
 select 'sequence name:  "' || sequence_name || '"' as sequences
 from user_sequences;
